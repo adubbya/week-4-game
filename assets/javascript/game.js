027b210275fa4c8163897ba9eb3f146120930b19
adubbya <aaron.mitchell@austin.utexas.edu>
2018-01-26 08:19:53 -0600

    multiple buttons, a bit of styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,45 @@
 $(document).ready(function () {
-        
-    // Game set
+
+
+
+    // VAIABLE        
         var randomNumber = [Math.floor(Math.random() * 120) + 19];
+        var crystalNumber = 0;
         var crystalCount = 0;
         var win = 0;
         var lose = 0;
         console.log(randomNumber);
         console.log(crystalCount);
     
-    // Crystal Click
-        $("#crystal-buttons").on("click", function () {
+    // Crystal Clicks, obv randomizes per click no bueno, need to randomize ONCE per click, per button
+        $("#crystal-button-1").on("click", function () {
+            var crystalNumber = [Math.floor(Math.random() * 12) + 1];
+            console.log(crystalNumber);
+            crystalCount = parseInt(crystalCount) + parseInt(crystalNumber);
+            $("#random-number").html(randomNumber);
+            $("#my-number").html(crystalCount);
+            console.log(crystalCount);
+        });
+
+        $("#crystal-button-2").on("click", function () {
+            var crystalNumber = [Math.floor(Math.random() * 12) + 1];
+            console.log(crystalNumber);
+            crystalCount = parseInt(crystalCount) + parseInt(crystalNumber);
+            $("#random-number").html(randomNumber);
+            $("#my-number").html(crystalCount);
+            console.log(crystalCount);
+        });
+
+        $("#crystal-button-3").on("click", function () {
+            var crystalNumber = [Math.floor(Math.random() * 12) + 1];
+            console.log(crystalNumber);
+            crystalCount = parseInt(crystalCount) + parseInt(crystalNumber);
+            $("#random-number").html(randomNumber);
+            $("#my-number").html(crystalCount);
+            console.log(crystalCount);
+        });
+
+        $("#crystal-button-4").on("click", function () {
             var crystalNumber = [Math.floor(Math.random() * 12) + 1];
             console.log(crystalNumber);
             crystalCount = parseInt(crystalCount) + parseInt(crystalNumber);
@@ -29,7 +59,7 @@ $(document).ready(function () {
         if (crystalCount > randomNumber) {
             $("#lose").text("you lose");
             lose += 1;
-            console.log("wellll", crystalCount. randomNumber)
+            console.log("wellll", crystalCount, randomNumber)
         //    reinit()    
         }
     
